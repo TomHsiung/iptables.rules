3842ab8775a618f48fe3aa6e525bc2eb15432cc8
TomHsiung <tom.hsiung@outlook.com>
2019-05-12 12:38:36 +0800

    Create iptables.ipsec.sh

---
+++ iptables.ipsec.sh
@@ -0,0 +1,2 @@
+iptables -t mangle -A FORWARD -m policy --pol ipsec --dir in -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
+iptables -t mangle -A FORWARD -m policy --pol ipsec --dir out -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:1536 -j TCPMSS --set-mss 1360
