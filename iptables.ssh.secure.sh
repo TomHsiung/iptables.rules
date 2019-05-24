528ad5c97d8353ac0a435435a4158fb7deb40ba3
TomHsiung <tom.hsiung@outlook.com>
2019-05-25 01:51:01 +0800

    Update iptables.ssh.secure.sh

--- iptables.ssh.secure.sh
+++ iptables.ssh.secure.sh
@@ -1,1 +1,1 @@
-sudo iptables -A INPUT -i eth0 -m tcp --protocol tcp --dport 22 -m hashlimit --hashlimit-above 1/min --hashlimit-mode srcip --hashlimit-name SSH -m state --state NEW -j REJECT
+iptables -A INPUT -i eth0 -m tcp --protocol tcp --dport 22 -m hashlimit --hashlimit-above 1/min --hashlimit-mode srcip --hashlimit-name SSH -m state --state NEW -j REJECT
