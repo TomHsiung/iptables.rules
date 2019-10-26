2c9dd051446641e7c9899ce2f86bacda51b4797c
TomHsiung <tom.hsiung@outlook.com>
2019-10-27 01:20:24 +0800

    Update iptables.tcpmss.clamp.sh

--- iptables.tcpmss.clamp.sh
+++ iptables.tcpmss.clamp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 iptables -t mangle -A FORWARD -o eth0 -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss --mss 1400:65495 -j TCPMSS --clamp-mss-to-pmtu
-iptables -t mangle -I INPUT 1 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
