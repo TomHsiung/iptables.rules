baa506b71b4265a3dc58f5e68d26cd5d85344523
TomHsiung <tom.hsiung@outlook.com>
2019-10-27 01:22:41 +0800

    Update iptables.ipsec.sh

--- iptables.ipsec.sh
+++ iptables.ipsec.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
+# tcpmss artificat modification in order to match MTU
 iptables -t mangle -I FORWARD 1 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
 iptables -t mangle -I FORWARD 2 -m policy --pol ipsec --dir out --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
+iptables -t mangle -I INPUT 1 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
 
 # Bruce force attack prevention exception for IPsec traffics
 iptables -t filter -I INPUT 1 -m policy --pol ipsec --dir in --protocol TCP --dport 22 -m state --state NEW -j ACCEPT
