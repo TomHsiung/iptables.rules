7f5231dced44ea8d277a1f90c5a80b55c018b9a9
TomHsiung <tom.hsiung@outlook.com>
2019-10-27 13:56:04 +0800

    Update iptables.ipsec.sh

--- iptables.ipsec.sh
+++ iptables.ipsec.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# tcpmss artificat modification in order to match MTU
+# tcpmss artificat modification in order to match MTU (site-to-site mode)
+iptables -t mangle -I FORWARD 1 -s 192.168.1.0/24 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1407:65495 -j TCPMSS --set-mss 1406
+iptables -t mangle -I FORWARD 2 -d 192.168.1.0/24 -m policy --pol ipsec --dir out --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1407:65495 -j TCPMSS --set-mss 1406
+iptables -t mangle -I INPUT 1 -s 192.168.1.0/24 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1407:65495 -j TCPMSS --set-mss 1406
+
+# tcpmss artificat modification in order to match MTU (roadwarrior mode)
 iptables -t mangle -I FORWARD 1 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
 iptables -t mangle -I FORWARD 2 -m policy --pol ipsec --dir out --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
 iptables -t mangle -I INPUT 1 -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
 
-# Bruce force attack prevention exception for IPsec traffics
+# Brute force attack prevention exception for IPsec traffics
 iptables -t filter -I INPUT 1 -m policy --pol ipsec --dir in --protocol TCP --dport 22 -m state --state NEW -j ACCEPT
 
 # Exception to masquerade rule(s) for packets about to pass through IPsec tunnel (strongSwan site-to-site mode)
