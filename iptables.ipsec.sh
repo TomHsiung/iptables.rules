7db14a7e2e4b6da77c2abc8da20e9ed90ca3c0c1
TomHsiung <tom.hsiung@outlook.com>
2019-10-31 12:28:27 +0800

    Update iptables.ipsec.sh

--- iptables.ipsec.sh
+++ iptables.ipsec.sh
@@ -2,10 +2,15 @@
 
 # set the appropriate direction of iptables rules based on the packet flow direction
 
+# tcpmss artificat modification in order to math MTU (two layers of tunnel)
+iptables -t mangle -I FORWARD 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1337:65495 -j TCPMSS --set-mss 1336
+iptables -t mangle -I FORWARD 2 -d x.x.x.x/x -m policy --pol ipsec --dir out --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1337:65495 -j TCPMSS --set-mss 1336
+iptables -t mangle -I INPUT 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1337:65495 -j TCPMSS --set-mss 1336
+
 # tcpmss artificat modification in order to match MTU (site-to-site mode)
-iptables -t mangle -I FORWARD 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1407:65495 -j TCPMSS --set-mss 1406
-iptables -t mangle -I FORWARD 2 -d x.x.x.x/x -m policy --pol ipsec --dir out --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1407:65495 -j TCPMSS --set-mss 1406
-iptables -t mangle -I INPUT 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1407:65495 -j TCPMSS --set-mss 1406
+iptables -t mangle -I FORWARD 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1399:65495 -j TCPMSS --set-mss 1398
+iptables -t mangle -I FORWARD 2 -d x.x.x.x/x -m policy --pol ipsec --dir out --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1399:65495 -j TCPMSS --set-mss 1398
+iptables -t mangle -I INPUT 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1399:65495 -j TCPMSS --set-mss 1398
 
 # tcpmss artificat modification in order to match MTU (roadwarrior mode)
 iptables -t mangle -I FORWARD 1 -s x.x.x.x/x -m policy --pol ipsec --dir in --protocol TCP --tcp-flags SYN,RST SYN -m tcpmss --mss 1361:65495 -j TCPMSS --set-mss 1360
