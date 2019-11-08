62698da69e246eb5f481bddca3da1d4873b97d87
TomHsiung <tom.hsiung@outlook.com>
2019-11-08 14:52:55 +0800

    Update iptables.ssh.secure.sh

--- iptables.ssh.secure.sh
+++ iptables.ssh.secure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 iptables -t filter -I INPUT 1 -m policy --pol ipsec --dir in --protocol TCP --dport 22 -m state --state NEW -j ACCEPT
-iptables -t filter -I INPUT 2 -i eth0 -m tcp --protocol tcp --dport 22 -j DROP
+iptables -t filter -I INPUT 2 -s x.x.x.x/x -i eth0 -m policy --pol ipsec --dir in --protocol tcp --dport 22 -j ACCEPT
+iptables -t filter -I INPUT 3 -i eth0 -m tcp --protocol tcp --dport 22 -j DROP
 iptables -A INPUT -i eth0 -m tcp --protocol tcp --dport 22 -m hashlimit --hashlimit-above 1/min --hashlimit-mode srcip --hashlimit-name SSH -m state --state NEW -j REJECT
