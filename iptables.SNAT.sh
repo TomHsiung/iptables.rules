4700e4a8209f9f3db339298af25a724c28e076b4
TomHsiung <tom.hsiung@outlook.com>
2019-12-12 15:22:11 +0800

    Update iptables.SNAT.sh

--- iptables.SNAT.sh
+++ iptables.SNAT.sh
@@ -2,7 +2,7 @@
 #For fixed gateway IP address
 iptables -t nat -I POSTROUTING 1 -p tcp -s x.x.x.x/x -o ethx -j SNAT --to-source y.y.y.y:x-x'
 iptables -t nat -I POSTROUTING 2 -p udp -s x.x.x.x/x -o ethx -j SNAT --to-source y.y.y.y:x-x'
-iptables -t nat -I POSTROUTING 3 --protocol ICMP -s x.x.x.x/x -o ethx -j SNAT --to-source y.y.y.y:x-x'
+iptables -t nat -I POSTROUTING 3 --protocol ICMP -s x.x.x.x/x -o ethx -j SNAT --to-source y.y.y.y
 
 #For dynamic gateway IP address
 iptables -t nat -I POSTROUTING 1 -d x.x.x.x/x -o eth0 -p udp -m multiport --dports x,y -j MASQUERADE --to-ports x-x'
